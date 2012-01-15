ffe36afca46ccbc5553040acebcdd6a527c38d6d
Nathan Mische <nmische@gmail.com>
2012-01-15 17:30:24 -0500

    Initial commit for AMD rewrite.

---
+++ src/firefox/chrome/content/cfPanel.js
@@ -0,0 +1,100 @@
+/* See license.txt for terms of usage */
+
+define([
+    "firebug/lib/object",
+    "firebug/lib/trace",
+    "firebug/lib/locale",
+    "firebug/lib/domplate"
+],
+function(Obj, FBTrace, Locale, Domplate) {
+
+// ********************************************************************************************* //
+// Custom Panel Implementation
+
+var panelName = "coldfire";
+
+function ColdFirePanel() {}
+ColdFirePanel.prototype = Obj.extend(Firebug.Panel,
+{
+    name: panelName,
+    title: "ColdFusion",
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+    // Initialization
+
+    initialize: function()
+    {
+        Firebug.Panel.initialize.apply(this, arguments);
+
+        if (FBTrace.DBG_COLDFIRE)
+            FBTrace.sysout("coldfire; ColdFirePanel.initialize");
+
+        // TODO: Panel initialization (there is one panel instance per browser tab)
+
+        this.refresh();
+    },
+
+    destroy: function(state)
+    {
+        if (FBTrace.DBG_COLDFIRE)
+            FBTrace.sysout("coldfire; ColdFirePanel.destroy");
+
+        Firebug.Panel.destroy.apply(this, arguments);
+    },
+
+    show: function(state)
+    {
+        Firebug.Panel.show.apply(this, arguments);
+
+        if (FBTrace.DBG_COLDFIRE)
+            FBTrace.sysout("coldfire; ColdFirePanel.show");
+    },
+
+    refresh: function()
+    {
+        // Render panel content. The HTML result of the template corresponds to: 
+        //this.panelNode.innerHTML = "<span>" + Locale.$STR("coldfire.panel.label") + "</span>";
+        this.ColdFireTemplate.render(this.panelNode);
+
+        // TODO: Render panel content
+    }
+});
+
+// ********************************************************************************************* //
+// Panel UI (Domplate)
+
+// Register locales before the following template definition.
+Firebug.registerStringBundle("chrome://coldfire/locale/coldfire.properties");
+
+/**
+ * Domplate template used to render panel's content. Note that the template uses
+ * localized strings and so, Firebug.registerStringBundle for the appropriate
+ * locale file must be already executed at this moment.
+ */
+with (Domplate) {
+ColdFirePanel.prototype.ColdFireTemplate = domplate(
+{
+    tag:
+        SPAN(
+            Locale.$STR("coldfire.panel.label")
+        ),
+
+    render: function(parentNode)
+    {
+        this.tag.replace({}, parentNode);
+    }
+})}
+
+// ********************************************************************************************* //
+// Registration
+
+Firebug.registerPanel(ColdFirePanel);
+Firebug.registerStylesheet("resource://coldfire/skin/classic/coldfire.css");
+
+if (FBTrace.DBG_COLDFIRE)
+    FBTrace.sysout("coldfire; cfPanel.js, stylesheet registered");
+
+return MyPanel;
+
+// ********************************************************************************************* //
+});
---
+++ src/firefox/chrome/content/main.js
@@ -0,0 +1,54 @@
+/* See license.txt for terms of usage */
+
+define([
+    "firebug/lib/trace",
+    "firebug/trace/traceModule",
+    "firebug/trace/traceListener",
+    "coldfire/cfPanel",
+    "coldfire/cfModule",
+],
+function(FBTrace, TraceModule, TraceListener) {
+
+// ********************************************************************************************* //
+// Documentation
+//
+// Firebug coding style: http://getfirebug.com/wiki/index.php/Coding_Style
+// Firebug tracing: http://getfirebug.com/wiki/index.php/FBTrace
+
+// ********************************************************************************************* //
+// The application/extension object
+
+var theApp =
+{
+    initialize: function()
+    {
+        // Register trace customization listener for FBTrace. DBG_COLDFIRE represents a CSS rule
+        // that is automatically associated with all logs prefixed with "coldfire;".
+        // The prefix is removed (third parameter is true).
+        // The last parameter represents URL of the stylesheet that should be used by
+        // the tracing console.
+        this.traceListener = new TraceListener("coldfire;", "DBG_COLDFIRE", true,
+            "resource://coldfire/skin/classic/coldfire.css");
+        TraceModule.addListener(this.traceListener);
+
+        if (FBTrace.DBG_COLDFIRE)
+            FBTrace.sysout("coldfire; ColdFire extension initialize");
+
+        // TODO: Extension initialization
+    },
+
+    shutdown: function()
+    {
+        if (FBTrace.DBG_COLDFIRE)
+            FBTrace.sysout("coldfire; ColdFire extension shutdown");
+
+        // TODO: Extension shutdown
+
+        TraceModule.removeListener(this.traceListener);
+    }
+}
+
+return theApp;
+
+// ********************************************************************************************* //
+});
